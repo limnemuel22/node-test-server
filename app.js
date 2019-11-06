ae1416675d5baa2b24b32788577f7bf0e21976fb
Nemuel <lim.nemuel.22@gmail.com>
2019-11-06 09:58:00 +0800

    First commit in node-test-server

---
+++ app.js
@@ -0,0 +1,96 @@
+const person = {
+    firstName: "Nemuel",
+    lastName: "Lim",
+    age: 28
+};
+//log an object
+console.log(person);
+
+//add function
+function addNumber(a, b) {
+    return a + b;
+}
+console.log(addNumber(7, 3));
+
+//print a log using variable with anonymous function
+const printBacon = () => {
+    console.log("Bacon is healthy, don't believe the doctors!");
+}
+printBacon();
+// set a schedule time for ouput data
+setTimeout(printBacon, 5000);
+
+
+function placeAnOrder(orderNumber) {
+    console.log("Customer order:", orderNumber);
+    cookAndDeliverFood(() => {
+        //   console.log("Delivered food order:", orderNumber);
+
+    })
+};
+
+function cookAndDeliverFood(callback) {
+    setTimeout(callback, 5000);
+}
+
+placeAnOrder(1);
+placeAnOrder(2);
+placeAnOrder(3);
+placeAnOrder(4);
+placeAnOrder(5);
+
+console.log(19 == '19'); // values only
+console.log(19 === '19'); // valaues and types
+
+//Reference to whatever thing is calling it
+const Nemuel = {
+    printFirstName: function () {
+        console.log("My Name is Nemuel");
+        console.log(this === Nemuel);
+    }
+};
+Nemuel.printFirstName();
+
+//The default calling context is global
+function doSomethingWorthless() {
+    console.log("\nI am worthless");
+    console.log(this === global);
+
+}
+doSomethingWorthless();
+
+function User() {
+    this.name = "";
+    this.life = 100;
+    this.giveLife = function giveLife(targetPlayer) {
+        targetPlayer.life += 1;
+        console.log(this.name + " gave 1 life to " + targetPlayer.name);
+    }
+}
+
+const Bucky = new User();
+var Wendy = new User();
+
+Bucky.name = "Bucky";
+Wendy.name = "Wendy";
+
+
+Bucky.giveLife(Wendy);
+console.log("Bucky: " + Bucky.life);
+console.log("Wendy: " + Wendy.life);
+
+// you can add function to all objects
+User.prototype.uppercut = function uppercut(targetPlayer) {
+    targetPlayer.life -= 3;
+    console.log(this.name + " just uppercutted " + targetPlayer.name);
+};
+
+Wendy.uppercut(Bucky);
+console.log("Bucky: " + Bucky.life);
+console.log("Wendy: " + Wendy.life);
+
+
+// you can add pproperties to all objects
+User.prototype.magic = 60;
+console.log(Bucky.magic);
+console.log(Wendy.magic);
